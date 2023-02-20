cc33f0c7836787ba3565628827c15531f1950d68
Prashant8khatiwada <prashantkhatiwada554@gmail.com>
2023-02-20 22:57:20 +0545

    api get request

--- src/api/FetchFromApi.jsx
+++ src/api/FetchFromApi.jsx
@@ -1,21 +1,15 @@
 import axios from "axios";
 
-const BASE_URL = "https://weatherbit-v1-mashape.p.rapidapi.com";
-const options = {
-  params: { lat: "35.5", lon: "-78.5" },
-  headers: {
-    "X-RapidAPI-Key": process.env.WEATHER_APP_KEY,
-    "X-RapidAPI-Host": "weatherbit-v1-mashape.p.rapidapi.com",
-  },
+// const frontUrl = process.env.BASE_URL;
+const frontUrl = `https://api.openweathermap.org/data/2.5/weather`;
+// const backUrl = process.env.BACK_URL;
+const backUrl = "&units=metric&appid=1bf779b770c0a107b0cce8a6eec0f09c";
+
+export const getData = async (data) => {
+  const queries = `?q=${data}`;
+  const url = frontUrl + queries + backUrl ;
+  const response = await axios.get(url);
+  return response;
 };
 
-export const FetchFromApi = async (url) => {
-  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-  return data;
-};
 
-// axios.request(options).then(function (response) {
-// 	console.log(response.data);
-// }).catch(function (error) {
-// 	console.error(error);
-// });
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,33 +1,43 @@
-import { React, useState, useEffect } from "react";
+import { React, useEffect, useState } from "react";
 import { BiSearch } from "react-icons/bi";
-import { FetchFromApi } from "../api/FetchFromApi";
+import { getData } from "../api/FetchFromApi";
+import { useFormik } from "formik";
 
 function SearchBar() {
-  const [searchTerm, setSearchTerm] = useState("");
   const [searchData, setSearchData] = useState([]);
 
-  console.log(searchData);
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const { values, handleChange, handleBlur } = useFormik({
+    initialValues: {
+      search: "",
+    },
+  });
+
+  const dataforApi = values.search;
+  const fetcgdata = () => {
+    getData(dataforApi).then((res) => {
+      setSearchData(res.data);
+    });
   };
 
-  useEffect(() => {
-    FetchFromApi(`current?part=snippet&q=${searchTerm}`).then((data) =>
-      setSearchData(data)
-    );
-  }, [handleSubmit]);
+  useEffect(fetcgdata, []);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    fetcgdata();
+  };
 
   return (
     <form className="search-bar">
       <input
+        id="search"
+        name="search"
         type="text"
         placeholder="Enter a City..."
-        value={searchTerm}
-        onChange={(e) => {
-          setSearchTerm(e.target.value);
-        }}
+        value={values.search}
+        onChange={handleChange}
+        onBlur={handleBlur}
       />
-      <button type="submit" className="search-icon" onClick={handleSubmit}>
+      <button type="submit" className="search-icon" onClick={handleClick}>
         <BiSearch />
       </button>
     </form>
